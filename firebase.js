4b3032414ef24bd2e58bf109b6572df61dc86bae
DoniDonka <89946478+DoniDonka@users.noreply.github.com>
2026-02-12 17:17:36 -0500

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,9 +1,16 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBLcIGjQUlprZSa4Xb4l_NirsBkZppiqJk",
-    authDomain: "text-d83ac.firebaseapp.com",
-    projectId: "text-d83ac",
-    storageBucket: "text-d83ac.firebasestorage.app",
-    messagingSenderId: "774345335493",
-    appId: "1:774345335493:web:160763b83ed48f633d86ac",
-    measurementId: "G-H29FGNXQZQ"
-  };
+// Your Firebase config
+var firebaseConfig = {
+    apiKey: "YOUR_KEY",
+    authDomain: "YOUR_DOMAIN",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_BUCKET",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// GLOBAL references
+const auth = firebase.auth();
+const db = firebase.firestore();
