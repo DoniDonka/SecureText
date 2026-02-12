abebf6844889126f56128a8fe5bad7265081a42e
DoniDonka <89946478+DoniDonka@users.noreply.github.com>
2026-02-12 17:43:51 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const error = document.getElementById("errorMsg");
 
 btn.onclick = () => {
     const name = input.value.trim();
+
     if (!name) {
         error.textContent = "Please enter a name.";
         return;
@@ -14,12 +15,10 @@ btn.onclick = () => {
         approved: false
     })
     .then(() => {
-        document.body.innerHTML = `
-            <div class="box">
-                <h1>SecureText</h1>
-                <p>Your name has been submitted.</p>
-                <p>Please wait for admin approval.</p>
-            </div>
+        document.getElementById("name-screen").innerHTML = `
+            <h1>SecureText</h1>
+            <p>Your name has been submitted.</p>
+            <p>Please wait for admin approval.</p>
         `;
     })
     .catch(() => {
